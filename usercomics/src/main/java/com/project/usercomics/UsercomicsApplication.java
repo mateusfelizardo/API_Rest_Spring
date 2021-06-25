c0b0227887a1fa1fddff26d04a8cd63ca5ab84f8
Mateus Felizardo <mateusfeliz159@gmail.com>
2021-06-25 14:42:30 -0300

    Inicio do Projeto

---
+++ usercomics/src/main/java/com/project/usercomics/UsercomicsApplication.java
@@ -0,0 +1,13 @@
+package com.project.usercomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercomicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercomicsApplication.class, args);
+	}
+
+}
