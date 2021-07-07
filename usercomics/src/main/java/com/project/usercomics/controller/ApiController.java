dbfc074ceb8626c42c5fa241ad7151633505e9a9
Mateus Felizardo <mateusfeliz159@gmail.com>
2021-07-06 22:13:25 -0300

    Add Swegger

--- usercomics/src/main/java/com/project/usercomics/controller/ApiController.java
+++ usercomics/src/main/java/com/project/usercomics/controller/ApiController.java
@@ -4,6 +4,8 @@ import com.project.usercomics.model.Comics;
 import com.project.usercomics.model.Usuario;
 import com.project.usercomics.repository.ComicsRepository;
 import com.project.usercomics.repository.UsuarioRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.Optional;
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api")
+@Api(value ="API REST USER_COMICS)")
 public class ApiController {
 
     @Autowired
@@ -22,27 +25,32 @@ public class ApiController {
     UsuarioRepository usuarioRepository;
 
     @GetMapping("/comics")
+    @ApiOperation(value = "Retorna uma lista de Comics")
     public List<Comics> listaComics(){
         return comicsRepository.findAll();
     }
 
     @PostMapping("/comics")
+    @ApiOperation(value = "Cadastra um Comics")
     public Comics salvarComic(@RequestBody Comics comics){
         return comicsRepository.save(comics);
     }
 
     @GetMapping("/usuarios")
+    @ApiOperation(value = "Retorna uma lista de Usuarios")
     public List<Usuario> listaUsuarios(){
         return usuarioRepository.findAll();
     }
 
     @GetMapping("/usuarios/{id}")
+    @ApiOperation(value = "Retorna um Usuarios pelo id")
     public Optional<Usuario> findUsuariosById(@PathVariable("id") long id){
 
         return usuarioRepository.findById(id);
     }
 
     @PostMapping("/usuarios")
+    @ApiOperation(value = "Cadastra um usuario")
     public Usuario salvarUsuario(@RequestBody Usuario usuario){
 
         return usuarioRepository.save(usuario);
